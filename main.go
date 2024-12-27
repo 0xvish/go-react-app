5794be73e0a05e96c226edfec74f086e9ba806d5
Vishvam Moliya <vishvammoliya@gmail.com>
2024-12-28 00:25:57 +0530

    feat(backend): boilerplate init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2/log"
+)
+
+func main() {
+	app := fiber.New()
+
+    app.Get("/", func (c *fiber.Ctx)  {
+        c.SendString("Hello, World!")
+    })
+
+    log.Fatal(app.Listen(":3000"))
+
+
+
+}
