756a7496b8ce5564fa7d4605f149908d0b0963b4
Vishvam Moliya <vishvammoliya@gmail.com>
2024-12-28 19:38:06 +0530

    feat(api): air setup, GET/POST todos to local memory

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
-	"github.com/gofiber/fiber/v2/log"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
+
+type Todo struct {
+    ID        int    `json:"id"`
+    Completed bool   `json:"completed"`
+    Task      string `json:"task"`
+}
 
 func main() {
 	app := fiber.New()
 
-    app.Get("/", func (c *fiber.Ctx)  {
-        c.SendString("Hello, World!")
+    todoList := []Todo{}
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.Status(200).JSON(fiber.Map{"message": "Hello, World!"})
+    })
+
+    app.Post("/api/todo", func(c *fiber.Ctx) error {
+        todo := &Todo{}
+
+        if err := c.BodyParser(todo); err !=nil {
+            return err
+        }
+
+        if todo.Task == "" {
+            return c.Status(400).JSON(fiber.Map{"error": "Task is required"})
+        }
+
+        todo.ID = len(todoList) + 1
+        todoList = append(todoList, *todo)
+
+        return c.Status(201).JSON(todo)
+    })
+
+    //return all todos
+    app.Get("/api/todo", func(c *fiber.Ctx) error {
+        return c.Status(200).JSON(todoList)
     })
 
     log.Fatal(app.Listen(":3000"))
